24b470638d7f7ca4fe33d56822428cc4d120967a
Cao Tan Duc <tanducsomeoneknow@gmail.com>
2023-07-10 14:13:36 +0700

    remove title world gdp

--- js/load-dashboards.js
+++ js/load-dashboards.js
@@ -1,3 +1,7 @@
+// center dashboard
+$("#world-gdp").load("./assets/dashboards/world-gdp.html");
+
+// detail elements
 $("#population-statistic").load("./assets/dashboards/population-statistic.html");
 $("#diff-in-labor").load("./assets/dashboards/diff-in-labor.html");
 $("#line-gdp").load("./assets/dashboards/line-gdp.html");
