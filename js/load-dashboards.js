ba653c7c0594e3ae5b56731efa15d6db9f04c2b6
Cao Tan Duc <tanducsomeoneknow@gmail.com>
2023-07-11 07:35:41 +0700

    growthest year chart update

--- js/load-dashboards.js
+++ js/load-dashboards.js
@@ -6,3 +6,4 @@ $("#gdp-vietnam-overview").load("./assets/dashboards/gdp-vietnam-overview.html")
 $("#population-statistic").load("./assets/dashboards/population-statistic.html");
 $("#labor-force").load("./assets/dashboards/labor-force.html");
 $("#indexes").load("./assets/dashboards/indexes.html");
+$("#three-growthest-year").load("./assets/dashboards/three-growthest-year.html");
